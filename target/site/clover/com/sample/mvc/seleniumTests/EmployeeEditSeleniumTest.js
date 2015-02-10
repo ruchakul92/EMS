e1f3980d3f5a54c86384a26c487d3a854b34d3da
rucha kul <ruchak@cybage.com>
2015-02-10 16:10:39 +0530

    selenium tests working

--- target/site/clover/com/sample/mvc/seleniumTests/EmployeeEditSeleniumTest.js
+++ target/site/clover/com/sample/mvc/seleniumTests/EmployeeEditSeleniumTest.js
@@ -4,7 +4,7 @@ var clover = new Object();
 clover.pageData = {"classes":[{"el":46,"id":97,"methods":[{"el":23,"sc":3,"sl":18},{"el":37,"sc":3,"sl":25},{"el":43,"sc":3,"sl":39}],"name":"EmployeeEditSeleniumTest","sl":15}]}
 
 // JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
-clover.testTargets = {"test_3":{"methods":[{"sl":25}],"name":"testEdit","pass":true,"statements":[{"sl":27},{"sl":29},{"sl":30},{"sl":31},{"sl":32},{"sl":33},{"sl":34}]}}
+clover.testTargets = {}
 
 // JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
-clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [3], [], [3], [], [3], [3], [3], [3], [3], [3], [], [], [], [], [], [], [], [], [], [], [], [], []]
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
