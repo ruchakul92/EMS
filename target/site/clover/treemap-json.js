8c1b33c576339f80518ec3dd68b945a241897c3b
rucha kul <ruchak@cybage.com>
2015-05-04 16:04:15 +0530

    5 May change

--- target/site/clover/treemap-json.js
+++ target/site/clover/treemap-json.js
@@ -1,5 +1,5 @@
-processTreeMapJson (  {"id":"Clover database Tue Feb 10 2015 16:07:11 IST0","name":"","data":{
-    "$area":56.0,"$color":0.0,"title":" 56 Elements, 0% Coverage"},
+processTreeMapJson (  {"id":"Clover database Wed Apr 29 2015 13:01:56 IST0","name":"","data":{
+    "$area":56.0,"$color":33.92857,"title":" 56 Elements, 33.9% Coverage"},
   "children":[{"id":"com.sample.mvc.service46","name":
       "com.sample.mvc.service","data":{"$area":10.0,"$color":0.0,"title":
         "com.sample.mvc.service 10 Elements, 0% Coverage"},"children":[{"id":
@@ -12,14 +12,21 @@ processTreeMapJson (  {"id":"Clover database Tue Feb 10 2015 16:07:11 IST0","nam
             "com/sample/mvc/service/EmployeeServiceImpl.html#EmployeeServiceImpl",
             "title":"EmployeeServiceImpl 10 Elements, 0% Coverage"},
           "children":[]}]},{"id":"com.sample.mvc.model34","name":
-      "com.sample.mvc.model","data":{"$area":12.0,"$color":0.0,"title":
-        "com.sample.mvc.model 12 Elements, 0% Coverage"},"children":[{"id":
+      "com.sample.mvc.model","data":{"$area":12.0,"$color":100.0,"title":
+        "com.sample.mvc.model 12 Elements, 100% Coverage"},"children":[{"id":
           "EmployeeCommand34","name":"EmployeeCommand","data":{"$area":
-            12.0,"$color":0.0,"path":
+            12.0,"$color":100.0,"path":
             "com/sample/mvc/model/EmployeeCommand.html#EmployeeCommand",
-            "title":"EmployeeCommand 12 Elements, 0% Coverage"},"children":[]}]},
-    {"id":"com.sample.mvc.dao19","name":"com.sample.mvc.dao","data":{"$area":
-        15.0,"$color":0.0,"title":
+            "title":"EmployeeCommand 12 Elements, 100% Coverage"},"children":
+          []}]},{"id":"com.sample.mvc.controller0","name":
+      "com.sample.mvc.controller","data":{"$area":19.0,"$color":36.842106,
+        "title":"com.sample.mvc.controller 19 Elements, 36.8% Coverage"},
+      "children":[{"id":"EmployeeController0","name":"EmployeeController",
+          "data":{"$area":19.0,"$color":36.842106,"path":
+            "com/sample/mvc/controller/EmployeeController.html#EmployeeController",
+            "title":"EmployeeController 19 Elements, 36.8% Coverage"},
+          "children":[]}]},{"id":"com.sample.mvc.dao19","name":
+      "com.sample.mvc.dao","data":{"$area":15.0,"$color":0.0,"title":
         "com.sample.mvc.dao 15 Elements, 0% Coverage"},"children":[{"id":
           "EmployeeDAO19","name":"EmployeeDAO","data":{"$area":0.0,"$color":
             -100.0,"path":
@@ -28,14 +35,6 @@ processTreeMapJson (  {"id":"Clover database Tue Feb 10 2015 16:07:11 IST0","nam
           "EmployeeDAOImpl19","name":"EmployeeDAOImpl","data":{"$area":
             15.0,"$color":0.0,"path":
             "com/sample/mvc/dao/EmployeeDAOImpl.html#EmployeeDAOImpl",
-            "title":"EmployeeDAOImpl 15 Elements, 0% Coverage"},"children":[]}]},
-    {"id":"com.sample.mvc.controller0","name":"com.sample.mvc.controller",
-      "data":{"$area":19.0,"$color":0.0,"title":
-        "com.sample.mvc.controller 19 Elements, 0% Coverage"},"children":[{
-          "id":"EmployeeController0","name":"EmployeeController","data":{
-            "$area":19.0,"$color":0.0,"path":
-            "com/sample/mvc/controller/EmployeeController.html#EmployeeController",
-            "title":"EmployeeController 19 Elements, 0% Coverage"},
-          "children":[]}]}]}
+            "title":"EmployeeDAOImpl 15 Elements, 0% Coverage"},"children":[]}]}]}
 
  ); 
